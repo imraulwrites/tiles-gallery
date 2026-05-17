fd95e8bd8a16936f31bf8609c1fe2296ffe3de4a
imraulwrites <imraul.dev@gmail.com>
2026-05-18 01:55:16 +0600

    updated domain name in auth files

--- app/lib/auth-client.js
+++ app/lib/auth-client.js
@@ -1,7 +1,7 @@
 import { createAuthClient } from 'better-auth/react';
 export const authClient = createAuthClient({
   /** The base URL of the server (optional if you're using the same domain) */
-  baseURL: 'https://tiles-gallery-sable.vercel.app/',
+  baseURL: 'https://tiles-gallery-m9lf0nnqm-imraulwrites-projects.vercel.app/',
 });
 
 export const { signIn, signUp, signOut, useSession } = authClient;
